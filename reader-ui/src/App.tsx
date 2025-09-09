4e7f12f881b74d400e2475b0bc82bab63832e90e
thomas <thoiceman@gmail.com>
2025-09-09 17:49:48 +0800

    feat:集成TypeScript

--- reader-ui/src/App.tsx
+++ reader-ui/src/App.tsx
@@ -1,10 +1,12 @@
 
-function App() {
+import React from 'react';
+
+const App: React.FC = () => {
   return (
     <div>
       <h1>Hello, React!</h1>
     </div>
   );
-}
+};
 
 export default App;
